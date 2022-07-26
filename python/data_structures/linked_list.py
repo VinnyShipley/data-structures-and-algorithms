ce5dbc6e66648fe902760ba34037e8e429f5ea38
Vinny <vinnyshipley@gmail.com>
2022-07-25 21:21:17 -0700

    append working, insert_before not

--- python/data_structures/linked_list.py
+++ python/data_structures/linked_list.py
@@ -30,6 +30,26 @@ class LinkedList:
             current = current.next
         return False
 
+    def append(self, value):
+        if self.head is None:
+            self.head = Node(value)
+        else:
+            current = self.head
+            if current.next is not None:
+                current = current.next
+            current.next = Node(value)
+
+    def insert_before(self, val, new_val):
+        current = self.head
+        while current.next != None:
+            if current.next.new_val == val:
+                current = Node(value = new_val)
+                current.next = current.next
+
+
+
+
+
 
 class Node:
     def __init__(self, value):
