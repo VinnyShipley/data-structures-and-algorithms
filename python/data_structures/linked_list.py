f2b4cb8d29efa5d40d7f2ac68ac6cacba734aa01
Vinny <vinnyshipley@gmail.com>
2022-07-26 20:30:42 -0700

    kth from end all passing

--- python/data_structures/linked_list.py
+++ python/data_structures/linked_list.py
@@ -6,6 +6,7 @@ class LinkedList:
     def __init__(self):
         self.head = None
 
+    # To string function
     def __str__(self):
         if self.head is None:
             return 'NULL'
@@ -17,11 +18,13 @@ class LinkedList:
             current = current.next
         return text + 'NULL'
 
+    # Insert Function
     def insert(self, val):
             old_head = self.head
             self.head = Node(val)
             self.head.next = old_head
 
+    # Includes function
     def includes(self, value):
         current = self.head
         while current:
@@ -30,6 +33,7 @@ class LinkedList:
             current = current.next
         return False
 
+    # Append function
     def append(self, value):
         if self.head is None:
             self.head = Node(value)
@@ -39,6 +43,7 @@ class LinkedList:
                 current = current.next
             current.next = Node(value)
 
+    # Insert before function
     def insert_before(self, val, new_val):
         current = self.head
         while current.next != None:
@@ -50,17 +55,22 @@ class LinkedList:
     def kth_from_end(self, k):
         length = 0
         current = self.head
+        if k < 0:
+            raise TargetError
         if current == None:
-            return 'error'
+            raise TargetError
         while current != None:
             length += 1
             current = current.next
         needed_steps = (length - 1) - k
+        if needed_steps < 0:
+            raise TargetError
         current = self.head
-        while needed_steps >= 0:
+        while needed_steps > 0:
             needed_steps -= 1
             current = current.next
-        return current
+        print(current)
+        return current.value
 
 
 class Node:
@@ -69,5 +79,5 @@ class Node:
         self.next = None
 
 
-class TargetError:
+class TargetError(Exception):
     pass
