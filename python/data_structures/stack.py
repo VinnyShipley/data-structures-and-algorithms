0651aee68a21054262f15efb6f4fc49f79285394
Vinny <vinnyshipley@gmail.com>
2022-07-29 17:43:35 -0700

    stack .py completed

--- python/data_structures/stack.py
+++ python/data_structures/stack.py
@@ -1,12 +1,41 @@
+from decimal import InvalidOperation
+from pickle import FALSE
+from data_structures.linked_list import Node
+from data_structures.invalid_operation_error import InvalidOperationError
+
 class Stack:
     """
     Put docstring here
     """
 
     def __init__(self):
-        # initialization here
-        pass
+        self.top = None
 
-    def some_method(self):
-        # method body here
-        pass
+    # push method
+    def push(self, value):
+        node = Node(value)
+        node.next = self.top
+        self.top = node
+
+    def pop(self):
+        if self.is_empty() == True:
+            raise InvalidOperationError('Method not allowed on empty collection')
+        temp = self.top
+        self.top = self.top.next
+        temp.next = None
+        return temp.value
+
+    def is_empty(self):
+        if self.top == None:
+            return True
+        else:
+            return False
+
+
+    def peek(self):
+        if self.is_empty() == True:
+            raise InvalidOperationError('Method not allowed on empty collection')
+        else:
+            return self.top.value
+
+
