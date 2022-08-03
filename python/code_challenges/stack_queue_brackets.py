aae65b3c281e1030d8e2ad0d90b64e122c5f1718
Vinny <vinnyshipley@gmail.com>
2022-08-03 15:13:58 -0700

    started, breaking for lab

--- python/code_challenges/stack_queue_brackets.py
+++ python/code_challenges/stack_queue_brackets.py
@@ -1,5 +1,24 @@
 from data_structures.queue import Queue
+import re
+
+wrong_tuple = ('[', '(', '{')
+
+def multi_bracket_validation(string):
+    regex = re.findall('[[|{|(|)|}|\]]', string)
+    for i in regex:
+        if regex[i] == '[' or '(' or '{':
+            check_bracket(regex[i], regex, i)
 
 
-def multi_bracket_validation():
-    pass
+
+def check_bracket(open, list, index):
+    index += 1
+    wrong_list_full = list(wrong_tuple)
+    if open == '[':
+
+    wrong_list_target =
+    for i in list:
+        i = list[i]
+        if i == '[' or '{' or '(':
+            check_bracket(i, list, index)
+        if i ==
