b88bdf74ec953778b8b8f4c6df6ed66320d1a42b
Vinny <vinnyshipley@gmail.com>
2022-08-03 23:45:13 -0700

    where its gonna be

--- python/code_challenges/stack_queue_brackets.py
+++ python/code_challenges/stack_queue_brackets.py
@@ -1,24 +1,39 @@
 from data_structures.queue import Queue
+from data_structures.stack import Stack
 import re
 
-wrong_tuple = ('[', '(', '{')
 
 def multi_bracket_validation(string):
-    regex = re.findall('[[|{|(|)|}|\]]', string)
-    for i in regex:
-        if regex[i] == '[' or '(' or '{':
-            check_bracket(regex[i], regex, i)
+    stack = Stack()
+    list = re.findall('[[|{|(|)|}|\]]', string)
 
-
-
-def check_bracket(open, list, index):
-    index += 1
-    wrong_list_full = list(wrong_tuple)
-    if open == '[':
-
-    wrong_list_target =
     for i in list:
-        i = list[i]
+
         if i == '[' or '{' or '(':
-            check_bracket(i, list, index)
-        if i ==
+            print(i)
+            stack.push(i)
+            break
+        if i == ']' or '}' or ')':
+
+            if stack.top == None:
+                return False
+            top_of_stack = stack.pop()
+            print('this is top of stack',top_of_stack)
+            if not char_matcher(top_of_stack, i):
+                return False
+    if len(stack) != 0:
+            return False
+    return True
+
+def char_matcher(opener, closer):
+    if opener == '(' and closer == ')':
+        return True
+
+    if opener == '{' and closer == '}':
+        return True
+
+    if opener == '[' and closer == ']':
+        return True
+
+    return False
+
