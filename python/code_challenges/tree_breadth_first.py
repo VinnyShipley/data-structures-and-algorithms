0e5f64b330baf8a40598b3126087d5cb67752ff3
Vinny <vinnyshipley@gmail.com>
2022-08-08 15:27:07 -0700

    all tests passing

--- python/code_challenges/tree_breadth_first.py
+++ python/code_challenges/tree_breadth_first.py
@@ -1,5 +1,32 @@
 from data_structures.binary_tree import BinaryTree
+from data_structures.queue import Queue
 
 
-def breadth_first():
-    pass
+def breadth_first(tree):
+    new_queue = Queue()
+    output_list = []
+    i = 1
+    root = tree.root
+
+    new_queue.enqueue(root)
+
+    if root is None:
+        return output_list
+
+    while not new_queue.is_empty():
+        print(new_queue)
+
+        print(i)
+        current = new_queue.dequeue()
+        output_list.append(current.value)
+        print(output_list)
+        if current.left:
+            new_queue.enqueue(current.left)
+            print(new_queue)
+
+        if current.right:
+            print(current.right.value)
+            new_queue.enqueue(current.right)
+            print(new_queue)
+        i += 1
+    return output_list
--- python/data_structures/queue.py
+++ python/data_structures/queue.py
@@ -30,16 +30,27 @@ class Queue:
     #removes the node from the front of the queue
     def dequeue(self):
 
-        #checks if queue is empty, and returns error if empty
-        if self.is_empty():
-            raise InvalidOperationError('InvalidOperationError')
+        # #checks if queue is empty, and returns error if empty
+        # if self.is_empty():
+        #     raise InvalidOperationError('InvalidOperationError')
 
-        # if non-empty list
-        else:
-            temp = self.front
-            self.front = self.front.next
-            temp.next = None
-            return temp.value
+        # # if non-empty list
+        # if self.front == self.rear:
+        #     self.rear is None
+        # else:
+        #     temp = self.front
+        #     self.front = self.front.next
+        #     temp.next = None
+        # return temp.value
+
+        if self.is_empty():
+            raise InvalidOperationError("InvalidOperationError")
+        prev_rear = self.front
+        if self.front is self.rear:
+            self.rear = None
+        self.front = self.front.next
+        prev_rear.next = None
+        return prev_rear.value
 
 
     #peek method
@@ -53,3 +64,13 @@ class Queue:
     # asserts that the front of the list is None. Is essentially a logic value to check against.
     def is_empty(self):
         return self.front is None
+
+    # def __str__(self):
+    #     temp = Queue()
+    #     string_rep = "Front -> "
+    #     while not self.is_empty():
+    #         curr = self.dequeue()
+    #         string_rep += f"{curr.value} -> "
+    #         temp.enqueue(curr)
+    #     self = temp
+    #     return string_rep
