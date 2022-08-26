cb4afcd0a9c1144a6d6f6f1cb1a369109ad57faa
Vinny <vinnyshipley@gmail.com>
2022-08-26 11:39:27 -0700

    start

--- python/data_structures/hashtable.py
+++ python/data_structures/hashtable.py
@@ -1,12 +1,40 @@
+from data_structures.linked_list import LinkedList
+
 class Hashtable:
     """
     Put docstring here
     """
 
-    def __init__(self):
-        # initialization here
-        pass
+    def __init__(self, size=1024):
+        self.size = size
+        self.list = [None] * self.size
 
-    def some_method(self):
-        # method body here
-        pass
+    def set(self, key, value):
+        key_value_tuple = (key, value)
+        index = self.hash(key)
+        if self.list[index] == None:
+            bucket_list = LinkedList()
+            self.list[index] = bucket_list
+            bucket_list.head = key_value_tuple
+        else:
+            while self.list[index]:
+                if
+
+
+    def hash(self, key):
+
+        char_num_list = []
+
+        for char in key:
+
+            num = ord(char)
+            primed = num * 599
+            char_num_list.append(primed)
+
+        char_sum = sum(char_num_list)
+        return self.size % char_sum
+
+
+
+
+
--- python/tests/data_structures/test_hashtable.py
+++ python/tests/data_structures/test_hashtable.py
@@ -1,34 +1,20 @@
 import pytest
 from data_structures.hashtable import Hashtable
+'''
+Setting a key/value to your hashtable results in the value being in the data structure
+Retrieving based on a key returns the value stored
+Successfully returns null for a key that does not exist in the hashtable
+Successfully returns a list of all unique keys that exist in the hashtable
+Successfully handle a collision within the hashtable
+Successfully retrieve a value from a bucket within the hashtable that has a collision
+Successfully hash a key to an in-range value
+'''
+
 
 
 def test_exists():
     assert Hashtable
 
+def test_null_key():
+    pass
 
-@pytest.mark.skip("TODO")
-def test_get_apple():
-    hashtable = Hashtable()
-    hashtable.set("apple", "Used for apple sauce")
-    actual = hashtable.get("apple")
-    expected = "Used for apple sauce"
-    assert actual == expected
-
-
-@pytest.mark.skip("TODO")
-def test_internals():
-    hashtable = Hashtable(1024)
-    hashtable.set("ahmad", 30)
-    hashtable.set("silent", True)
-    hashtable.set("listen", "to me")
-
-    actual = []
-
-    # NOTE: purposely breaking encapsulation to test the "internals" of Hashmap
-    for item in hashtable._buckets:
-        if item:
-            actual.append(item.display())
-
-    expected = [[["silent", True], ["listen", "to me"]], [["ahmad", 30]]]
-
-    assert actual == expected
