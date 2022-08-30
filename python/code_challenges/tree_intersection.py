2ecdb8f76cf92e2db774ba83ecf5124c3074701e
Vinny <vinnyshipley@gmail.com>
2022-08-30 13:16:33 -0700

    code working

--- python/code_challenges/tree_intersection.py
+++ python/code_challenges/tree_intersection.py
@@ -1,5 +1,20 @@
 from data_structures.binary_tree import BinaryTree
+from data_structures.hashtable import Hashtable
 
 
-def tree_intersection():
-    pass
+
+
+def tree_intersection(tree_a, tree_b):
+    hash = Hashtable()
+    return_nums = []
+    a_values = tree_a.pre_order()
+    b_values = tree_b.pre_order()
+    for nums in a_values:
+        key = str(nums)
+        hash.set(key, nums)
+    for nums in b_values:
+        key = str(nums)
+        if hash.contains(key) and not nums in return_nums:
+            return_nums.append(nums)
+    print(return_nums)
+    return return_nums
