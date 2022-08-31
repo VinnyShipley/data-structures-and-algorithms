29075632c44f519dd9ac612fffa0105c0cf33621
Vinny <vinnyshipley@gmail.com>
2022-08-31 14:50:42 -0700

    formatted

--- python/code_challenges/hashtable_left_join.py
+++ python/code_challenges/hashtable_left_join.py
@@ -1,5 +1,16 @@
 from data_structures.hashtable import Hashtable
 
+def left_join(table_a, table_b):
 
-def left_join():
-    pass
+    return_table = []
+    a_keys = table_a.keys()
+
+    for key in a_keys:
+
+        if table_b.contains(key):
+            return_table.append([key, table_a.get(key), table_b.get(key)])
+
+        else:
+            return_table.append([key, table_a.get(key), None])
+
+    return return_table
