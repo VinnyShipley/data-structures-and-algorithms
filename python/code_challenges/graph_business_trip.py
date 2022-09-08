bf9bb03c4d85f56d86a6483892a1a55d5ac664f7
Vinny <vinnyshipley@gmail.com>
2022-09-07 19:43:34 -0700

    9/7

--- python/code_challenges/graph_business_trip.py
+++ python/code_challenges/graph_business_trip.py
@@ -1,5 +1,18 @@
 from data_structures.graph import Graph
 
 
-def direct_flights():
-    pass
+def direct_flights(graph, cities):
+    cost = 0
+
+    for i in (0, len(cities) - 1):
+
+        neighbor_vertexes = []
+        nodes = set(graph.get_nodes())
+
+        for edge in graph.get_neighbors(nodes(cities[i])):
+            neighbor_vertexes.append(edge[cities[i]])
+
+        if cities[i + 1] in neighbor_vertexes:
+            routes = graph.get_neighbors(cities[i])
+            cost += routes[cities[i + 1].weight]
+
